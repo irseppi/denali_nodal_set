db054e60921e14c97c7a137903fc1333ae6213cc
irseppi <117307489+irseppi@users.noreply.github.com>
2023-06-12 16:42:50 -0800

    Update 50sps_missing.py

--- 50sps_missing.py
+++ 50sps_missing.py
@@ -40,13 +40,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-02-23
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DPZ&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1101-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DPZ&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DPZ&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DPZ&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DPZ&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DPZ&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
@@ -58,13 +58,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-03-01
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DPZ&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1102-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DPZ&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DPZ&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DPZ&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DPZ&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DPZ&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
@@ -80,13 +80,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-02-13
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP1&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1101-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP1&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP1&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP1&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP1&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP1&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
@@ -98,13 +98,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-02-20
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP1&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1101-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP1&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP1&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP1&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP1&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP1&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
@@ -117,13 +117,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-02-23
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP1&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1101-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP1&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP1&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP1&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP1&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP1&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
@@ -135,13 +135,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-03-01
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP1&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1102-1199
+#1100-1199
 #wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP1&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP1&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP1&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP1&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP1&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
@@ -154,13 +154,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-02-13
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP2&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1101-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP2&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP2&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP2&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP2&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP2&starttime=2019-02-13T00:00:00&endtime=2019-02-14T00:00:00&decimation=10&nodata=404" 
@@ -172,13 +172,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-02-20
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP2&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1101-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP2&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP2&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP2&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP2&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP2&starttime=2019-02-20T00:00:00&endtime=2019-02-21T00:00:00&decimation=10&nodata=404" 
@@ -191,13 +191,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-02-23
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP2&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1101-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP2&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP2&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP2&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP2&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP2&starttime=2019-02-23T00:00:00&endtime=2019-02-24T00:00:00&decimation=10&nodata=404" 
@@ -209,13 +209,13 @@ wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=msee
 #2019-03-01
 #1001-1099
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=10*&cha=DP2&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1102-1199
+#1100-1199
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=11*&cha=DP2&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1201-1299
+#1200-1299
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=12*&cha=DP2&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1301-1399
+#1300-1306
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=13*&cha=DP2&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
-#1501-1589
+#1500-1589
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=15*&cha=DP2&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
 #5510, 5511, 5575, 5900
 wget "https://service.iris.edu/ph5ws/dataselect/1/query?reqtype=FDSN&format=mseed&net=ZE&reportnum=19-007&sta=5*&cha=DP2&starttime=2019-03-01T00:00:00&endtime=2019-03-02T00:00:00&decimation=10&nodata=404" 
