33751bac00db4e8e26e4befd2ae4f54853e63927
irseppi <117307489+irseppi@users.noreply.github.com>
2023-06-26 11:40:24 -0800

    Update reformat_tracks.py

--- reformat_tracks.py
+++ reformat_tracks.py
@@ -1,3 +1,6 @@
+#after converting Alaska Railway kml file to text files reformat to be just lat and lon
+#https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwi5tuT-_MP_AhUSJ30KHYmMDv0QFnoECAoQAQ&url=https%3A%2F%2Fwww.nps.gov%2Fdena%2Fplanyourvisit%2Fupload%2FAlaska%2520Railroad.kml&usg=AOvVaw2bKtne-33MDOx0zuXJjtR6
+
 import sys
 import fileinput
 
