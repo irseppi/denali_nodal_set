2a6725b878560364f48907861fac5c903f4e0728
irseppi <117307489+irseppi@users.noreply.github.com>
2023-06-26 11:41:03 -0800

    Update reformat_tracks.py

--- reformat_tracks.py
+++ reformat_tracks.py
@@ -4,7 +4,7 @@
 import sys
 import fileinput
 
-# replace all occurrences of 'sit' with 'SIT' and insert a line after the 5th
+# replace all occurrences of ', 10' with '\n' 
 for i, line in enumerate(fileinput.input('Alaska_Railroad.txt', inplace=1)):
     sys.stdout.write(line.replace(',', '   ')) 
     sys.stdout.write(line.replace(', 10 ', '\n')) 
