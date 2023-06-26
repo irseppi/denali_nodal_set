e82179c67e1259bf678af3c8ebdeb4b72f117d06
irseppi <117307489+irseppi@users.noreply.github.com>
2023-06-26 11:33:24 -0800

    Add files via upload

---
+++ reformat_road.py
@@ -0,0 +1,9 @@
+import sys
+import fileinput
+
+for i, line in enumerate(fileinput.input('PARKSHIGHWAY3.txt', inplace=1)):
+    #sys.stdout.write(line.replace(',', '   ')) 
+    sys.stdout.write(line.replace(',0', '\n')) 
+    #sys.stdout.write(line.replace('  0', '  '))
+    sys.stdout.write(line.replace('</coordinates>..<coordinates>', '  '))
+
