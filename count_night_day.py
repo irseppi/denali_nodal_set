26231b6005070ce37630198066706b6abff13a1d
irseppi <117307489+irseppi@users.noreply.github.com>
2023-09-18 17:16:32 -0800

    Update count_night_day.py

--- count_night_day.py
+++ count_night_day.py
@@ -1,87 +1,50 @@
+from datetime import datetime
+from collections import Counter
 import matplotlib.pyplot as plt
-from obspy.core import UTCDateTime
-import datetime
-from datetime import datetime, timedelta
-import pytz
 
-# open file in read mode
-text = open("nodal_ZE.arrival", "r")
+# Read the timestamps from the text file
+with open('dates2.txt', 'r') as f:
+    timestamps = f.read().splitlines()
 
-day_data=[]	
-night_data=[]
-num = 0
+# Convert the timestamps to datetime objects
+datetimes = [datetime.strptime(ts, '%Y-%m-%d %H:%M:%S') for ts in timestamps]
 
-for line in text.readlines():
-	val = line.split()
-	timestamps = UTCDateTime(float(val[1]))
+# Filter the datetimes to only include those between 8 and 15 UTC
+filtered_datetimes = [dt for dt in datetimes if 8 >= dt.hour or dt.hour > 15]
 
-	for x in range(11,29):
+# Count the number of dates for each day
+date_counts = Counter(dt.date() for dt in filtered_datetimes)
 
-		if x <= 28:
-			# Set the start and end times for the day and night periods
-			day_start = UTCDateTime("2019-02-"+str(x-1)+"T15:00:00")
-			day_end = UTCDateTime("2019-02-"+str(x)+"T08:08:00")
-			night_start = UTCDateTime("2019-02-"+str(x)+"T08:00:00")
-			night_end = UTCDateTime("2019-02-"+str(x)+"T15:00:00")
-
-			# Filter the data to only include the day and night periods
-			if day_start <= timestamps < day_end:
-				day_data.append(timestamps)
-			if night_start <= timestamps < night_end:
-				night_data.append(timestamps)
-
-	for y in range(2,27):
-		day_start = UTCDateTime("2019-03-"+str(y-1)+"T15:00:00")
-		day_end = UTCDateTime("2019-03-"+str(y)+"T08:00:00")
-		night_start = UTCDateTime("2023-03-"+str(y)+"T08:00:00")
-		night_end = UTCDateTime("2023-03-"+str(y)+"T15:00:00")
-
-		# Filter the data to only include the day and night periods
-		if day_start <= timestamps < day_end:
-			day_data.append(timestamps)
-		if night_start <= timestamps < night_end:
-			night_data.append(timestamps)
-
-	day_start = UTCDateTime("2019-02-28T15:00:00")
-	day_end = UTCDateTime("2019-02-01T08:00:00")
-	night_start = UTCDateTime("2023-03-01T08:00:00")
-	night_end = UTCDateTime("2023-03-01T15:15:00")
-	if day_start <= str(timestamps) < day_end:
-		day_data.append(timestamps)
-	if night_start <= str(timestamps) < night_end:
-		night_data.append(timestamps)	
-	num = num + 1
-
-# Calculate the arrival times for the day and night periods
-day_arrivals = len(day_data)
-night_arrivals = len(night_data)
-
-# Compare the arrival times between day and night
-print("# of Day arrivals:", day_arrivals)
-print("# of Night arrivals:", night_arrivals)
-print("Total # of picks", num)
-
-# Convert UTCDateTime objects to datetime objects
-day_dates = [datetime.utcfromtimestamp(date.timestamp) for date in day_data]
-night_dates = [datetime.utcfromtimestamp(date.timestamp) for date in night_data]
-
-# Define the local time zone
-local_tz = pytz.timezone('US/Alaska')
-
-# Convert UTC datetimes to local datetimes
-local_day = [local_tz.normalize(day_date.replace(tzinfo=pytz.utc).astimezone(local_tz)) for day_date in day_dates]
-local_night = [local_tz.normalize(night_date.replace(tzinfo=pytz.utc).astimezone(local_tz)) for night_date in night_dates]
-
-# Create histogram
-plt.hist(local_day, bins=43)
+# Create a histogram from the date counts
+plt.bar(date_counts.keys(), date_counts.values())
 plt.title('Day Time Arrival Count')
 plt.xlabel('Date')
 plt.ylabel('Count')
-#plt.xticks([UTCDateTime(day_dates).strftime("%Y-%m-%d") for date in range(min(night_dates), max(night_dates) + 2)], rotation=45)
+plt.show()
+	
+
+# Read the timestamps from the text file
+with open('dates2.txt', 'r') as f:
+    timestamps = f.read().splitlines()
+
+# Convert the timestamps to datetime objects
+datetimes = [datetime.strptime(ts, '%Y-%m-%d %H:%M:%S') for ts in timestamps]
+
+# Filter the datetimes to only include those between 8 and 15 UTC
+filtered_datetimes = [dt for dt in datetimes if 8 <= dt.hour < 15]
+
+# Count the number of dates for each day
+date_counts = Counter(dt.date() for dt in filtered_datetimes)
+
+# Create a histogram from the date counts
+plt.bar(date_counts.keys(), date_counts.values())
+plt.title('Night Time Arrival Count')
+plt.xlabel('Date')
+plt.ylabel('Count')
 plt.show()
 
-plt.hist(local_night, bins=43)
-plt.title('Night Time Arrival Count')
+plt.hist(datetimes, bins=43)
+plt.title('All Time Arrival Count')
 plt.xlabel('Date')
 plt.ylabel('Count')
 plt.show()
